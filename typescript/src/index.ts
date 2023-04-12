76437a949e58a77bb27f290052b97b34451d7d0f
Cory Codling <ccodling@navex.com>
2023-04-12 14:33:50 -0600

    try with helpers.js

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -16,14 +16,12 @@
     }
 
     // Create "sentence" separated by spaces so we can camelcase it all
-    let sentence = segments.join("-");
+    let sentence = segments.join(" ");
 
     // camelcase string from all segments
-    //  sentence = sentence
-    //   .toLowerCase()
-    //   .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
-
-    sentence = sentence.replace(/[A-Z]/g, m => "-" + m.toLowerCase());
+     sentence = sentence
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
     
     // only allow letters, digits, underscore and hyphen
     sentence = sentence.replace(/[^a-zA-Z0-9_-]/g, '_')
