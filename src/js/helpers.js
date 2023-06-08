792b096f7b71b82d6f816b200ed16395bd2d57eb
Cory Codling <ccodling@navexglobal.com>
2023-06-08 17:50:05 -0600

    Config using Pulsar

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -6,14 +6,14 @@ Pulsar.registerFunction(
   function(token, tokenGroup) {
     // Create array with all path segments and token name at the end
     const segments = [...tokenGroup.path];
-    const prefix = "nds"
+    //const prefix = "nds"
     if (!tokenGroup.isRoot) {
       //segments.push(tokenGroup.name)
     }
 
     segments.push(token.name);
 
-    segments.unshift(prefix);
+    segments.unshift(Pulsar.tokenPrefix);
 
     // Create "sentence" separated by spaces
     let sentence = segments.join(" ");
